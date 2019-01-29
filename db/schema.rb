bbcc2dae86a06e0cd13b9485871b2f78f6279e42
Keren-Happuc Fleurimond <keren@noukod.com>
2019-01-29 17:08:50 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -13,20 +13,12 @@
 
 ActiveRecord::Schema.define(version: 1) do
   create_table "appointments", force: :cascade do |t|
-
    t.datetime "appointment_datetime"
-
    t.integer  "doctor_id"
-
    t.integer  "patient_id"
-
    t.datetime "created_at",           null: false
-
    t.datetime "updated_at",           null: false
-
  end
-
-
 
  create_table "doctors", force: :cascade do |t|
    t.string   "name"
